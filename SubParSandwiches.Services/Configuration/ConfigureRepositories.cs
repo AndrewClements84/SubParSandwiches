ee824887d7dc040ee104ab0b671c72ac4caaf728
Andrew <andrew.morgan@e.org>
2022-01-02 19:30:29 +0000

    Added Repository Configuration File

---
+++ SubParSandwiches.Services/Configuration/ConfigureRepositories.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SubParSandwiches.Entities;
+using SubParSandwiches.Repositories;
+using SubParSandwiches.Repositories.Implementations;
+using SubParSandwiches.Repositories.Interfaces;
+
+namespace SubParSandwiches.Services.Configuration
+{
+    public class ConfigureRepositories
+    {
+        public static void AddServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>((options) =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DbConnection"));
+            });
+            services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+            services.AddScoped<DbContext, AppDbContext>();
+
+            services.AddTransient<ICartRepository, CartRepository>();
+            services.AddTransient<IRepository<Item>, Repository<Item>>();
+            services.AddTransient<IRepository<Category>, Repository<Category>>();
+            services.AddTransient<IRepository<ItemType>, Repository<ItemType>>();
+            services.AddTransient<IRepository<CartItem>, Repository<CartItem>>();
+            services.AddTransient<IRepository<PaymentDetails>, Repository<PaymentDetails>>();
+        }
+    }
+}
