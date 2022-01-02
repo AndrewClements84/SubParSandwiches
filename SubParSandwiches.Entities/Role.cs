11e698452b5a9a4874ad6be98c8fc0555dd3a959
Andrew <andrew.morgan@e.org>
2022-01-02 17:24:57 +0000

    Add project files.

---
+++ SubParSandwiches.Entities/Role.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SubParSandwiches.Entities
+{
+    public class Role : IdentityRole<int>
+    {
+        public string Description { get; set; }
+    }
+}
