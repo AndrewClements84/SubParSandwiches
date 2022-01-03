72940f3bd02075add836ff6ff867ef110606fed4
Andrew <andrew.morgan@e.org>
2022-01-03 12:23:05 +0000

    Added Base Controller

---
+++ SubParSandwiches.WebUI/Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SubParSandwiches.Entities;
+using SubParSandwiches.WebUI.Interfaces;
+
+namespace SubParSandwiches.WebUI.Controllers
+{
+    public class BaseController : Controller
+    {
+        public User CurrentUser
+        {
+            get
+            {
+                if (User != null)
+                    return _userAccessor.GetUser();
+                else
+                    return null;
+            }
+        }
+
+        IUserAccessor _userAccessor;
+        public BaseController(IUserAccessor userAccessor)
+        {
+            _userAccessor = userAccessor;
+        }
+    }
+}
