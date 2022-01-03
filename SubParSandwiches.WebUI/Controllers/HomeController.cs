faed439ee05bf60de360e85a12077c129bf5befb
Andrew <andrew.morgan@e.org>
2022-01-03 12:21:33 +0000

    Added Home and Account Controllers

--- SubParSandwiches.WebUI/Controllers/HomeController.cs
+++ SubParSandwiches.WebUI/Controllers/HomeController.cs
@@ -1,26 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SubParSandwiches.Services.Interfaces;
 using SubParSandwiches.WebUI.Models;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SubParSandwiches.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        ICatalogService _catalogService;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICatalogService catalogService)
         {
             _logger = logger;
+            _catalogService = catalogService;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var items = _catalogService.GetItems();
+            return View(items);
         }
 
         public IActionResult Privacy()
